ea2b06dec7da2dceff7685eff6a7ac10fb4079f1
Vishal <100362137+Vishalsi23ngh@users.noreply.github.com>
2024-05-12 17:10:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function indexOfIgnoreCase(s1, s2) {
 	let ans = -1;
 	let val = s2.charAt(0);
 	for(let i = 0; i < s1.length; i++){
-		if(val == s2.charAt(i)){
+		if(val.toLowerCase() === s2.charAt(i).toLowerCase()){
 			ans = i;
 		}
 	}
@@ -11,6 +11,6 @@ function indexOfIgnoreCase(s1, s2) {
 }
 
 // Please do not change the code below
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(indexOfIgnoreCase(s1, s2));
+// const s1 = prompt("Enter s1:");
+// const s2 = prompt("Enter s2:");
+// alert(indexOfIgnoreCase(s1, s2));
